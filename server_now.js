96ca90a21a3c53e95f80ac46f37a446b787f44cf
Arthur Pachachura <pachachura.arthur@gmail.com>
2016-07-24 16:41:54 -0500

    Seems to work

--- server_now.js
+++ server_now.js
@@ -4,7 +4,10 @@ var moment = require('moment');
 
 exports.getEvents = function(session, conceptUri, lang, cb)
 {
-  request('http://eventregistry.org/json/event?action=getEvents&resultType=events&dateStart=2016-07-09&dateEnd=2016-07-23&eventsConceptLang=eng&eventsCount=200&eventsEventImageCount=1&minArticlesInEvent=10&eventsIncludeEventSocialScore=true&conceptUri=' + conceptUri + '&eventsSortBy=rel&eventsPage=1', { jar: session }, function(error, response, body) {
+  //&dateStart=2016-07-09 <- for recent hits only
+  /**For the most part, getting first 50 articles by relevance is pretty good - however, for extremely large sets,
+     it may be necessary to load another 200-400 articles. **/
+  request('http://eventregistry.org/json/event?action=getEvents&resultType=events&eventsConceptLang=eng&eventsCount=50&eventsEventImageCount=1&minArticlesInEvent=10&eventsIncludeEventSocialScore=true&conceptUri=' + conceptUri + '&eventsSortBy=rel&eventsPage=1', { jar: session }, function(error, response, body) {
     var out = { status: 500, error: undefined, events: [ ]};
     if (error || JSON.parse(body).error !== undefined) {
       console.error(body);
@@ -27,53 +30,63 @@ exports.getEvents = function(session, conceptUri, lang, cb)
 
       //Rate each event and append to array
       var maxRelevance = json.events.results[0].wgt;
-      async.eachLimit(json.events.results, 8, function(event, cb)
+      async.eachLimit(json.events.results, 8, function(event, asynccb)
       {
         //Get metadata
-        var eventout = { };
-        eventout.uri = event.uri;
-        eventout.title = event.title[lang] || event.title["eng"] || "No title provided";
-        if (event.location === undefined || event.location == null || event.location.length == 0) { eventout.location = "" }
-        else {
-          eventout.location = event.location.label[lang] || event.location.label["eng"] || "";
-          if (event.location.country !== undefined) eventout.location += ", " + (event.location.country.label[lang] || event.location.country.label["eng"] || "");
-        }
-        eventout.time = moment(event.eventDate, "YYYY-MM-DD").fromNow();
-
-        //Parse summary
-        eventout.summary = event.summary[lang] || "No summary provided.";
-        eventout.summaryShort = eventout.summary.match(/^.+?\.|^.*/)[0];
-        if (eventout.summaryShort.length > 400)
+        if (event.warning === undefined)
         {
-          eventout.summaryShort = eventout.summaryShort.match(/^.{0,400}\b/)[0].trim();
-          eventout.summaryShort += "...";
-        }
-
-        //Get article counts
-        eventout.articlesTotal = 0;
-        eventout.articlesLang = event.articleCounts[lang] || event.articleCounts["eng"] || 0;
-        eventout.languages = event.articleCounts.length;
-        for(key in event.articleCounts) {
-          if(event.articleCounts.hasOwnProperty(key)) {
-            eventout.articlesTotal += event.articleCounts[key];
+          var eventout = { };
+          eventout.uri = event.uri;
+          eventout.title = event.title[lang] || event.title["eng"] || null;
+          if (event.location === undefined || event.location == null || event.location.length == 0) { eventout.location = "" }
+          else {
+            eventout.location = event.location.label[lang] || event.location.label["eng"] || "";
+            if (event.location.country !== undefined) eventout.location += ", " + (event.location.country.label[lang] || event.location.country.label["eng"] || "");
           }
-        }
+          eventout.time = moment(event.eventDate, "YYYY-MM-DD").fromNow();
+          eventout.image = event.images[0] || null;
 
-        //Calculate overall hotness
-        var searchRelevance = event.wgt;
-        var socialScore = event.socialScore;
-        var timeDiffHours = moment(new Date()).diff(moment(event.eventDate), 'hours');
-        var recencyFactor = 10.2273515 / ((timeDiffHours)^(0.5494897));
-        var relevanceFactor = searchRelevance / maxRelevance;
-        var socialFactor = 0.00772985 * ((socialScore)^(0.787861));
-        var newsFactor = 0.00772985 * ((eventout.articlesLang)^(0.787861));
+          //Parse summary
+          eventout.summary = event.summary[lang] || event.summary["eng"] || null;
+          eventout.summaryShort = eventout.summary != null ? eventout.summary.match(/^.+?\.|^.*/)[0] : null;
+          if ((eventout.summaryShort != null) && (eventout.summaryShort.length > 400))
+          {
+            eventout.summaryShort = eventout.summaryShort.match(/^.{0,400}\b/)[0].trim();
+            eventout.summaryShort += "...";
+          }
 
-        eventout.hotness = 25 * recencyFactor * (socialFactor + newsFactor);
-        eventout.relevance = eventout.hotness * relevanceFactor;
+          //Get article counts
+          eventout.articlesTotal = 0;
+          eventout.articlesLang = event.articleCounts[lang] || event.articleCounts["eng"] || 0;
+          eventout.languages = event.articleCounts.length;
+          for(key in event.articleCounts) {
+            if(event.articleCounts.hasOwnProperty(key)) {
+              eventout.articlesTotal += event.articleCounts[key];
+            }
+          }
 
-        out.events.push(eventout);
-        cb();
+          //Calculate overall hotness
+          var searchRelevance = event.wgt;
+          var socialScore = event.socialScore;
+          var timeDiffHours = moment(new Date()).diff(moment(event.eventDate), 'hours');
+          var recencyFactor = 10.2273515 / ((timeDiffHours)^(0.5494897));
+          var relevanceFactor = searchRelevance / maxRelevance;
+          var socialFactor = 0.01772985 * ((socialScore)^(0.787861));
+          var newsFactor = 0.01772985 * ((eventout.articlesLang)^(0.787861));
 
+          eventout.hotness = Math.ceil(25 * recencyFactor * (socialFactor + newsFactor));
+          eventout.relevance = eventout.hotness * relevanceFactor;
+          console.log("SocBase: " + socialScore + " Time: " + recencyFactor + " Soc: " + socialFactor + " News: " + newsFactor + "SocF: " + socialFactor + " Score: " + eventout.relevance);
+
+          if (eventout.summary == null || eventout.relevance == 0 || eventout.title == null)
+          {
+            asynccb();
+          }
+          else {
+            out.events.push(eventout);
+            asynccb();
+          }
+        } else { asynccb(); }
       }, function (err) {
         console.log("Done analyzing events!");
 
