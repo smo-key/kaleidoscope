c8f7a9b0db8c88b3b671174f0d7ff0b80a785e70
Arthur Pachachura <pachachura.arthur@gmail.com>
2016-07-25 16:56:36 -0500

    Slightly improve event search again

--- server_now.js
+++ server_now.js
@@ -6,30 +6,47 @@ exports.getEvents = function(session, conceptUri, lang, cb)
 {
   //&dateStart=2016-07-09 <- for recent hits only
   /**For the most part, getting first 50 articles by relevance is pretty good - however, for larger items, also get the most *recent* articles **/
-  request('http://eventregistry.org/json/event?action=getEvents&resultType=events&eventsConceptLang=eng&eventsCount=50&eventsEventImageCount=1&minArticlesInEvent=10&eventsIncludeEventSocialScore=true&conceptUri=' + conceptUri + '&eventsSortBy=rel&eventsPage=1', { jar: session }, function(error, response, body) {
-    var out = { status: 500, error: undefined, events: [ ]};
-    if (error || JSON.parse(body).error !== undefined) {
-      console.error(body);
-      console.error(error);
-      out.status = (JSON.parse(body).error !== undefined) ? 200 : 500;
-      out.events = (JSON.parse(body).error !== undefined) ? [ ] : undefined;
-      out.error = error || JSON.parse(body).error;
-      cb(out);
+  async.parallel([
+    function(callback) {
+      request('http://eventregistry.org/json/event?action=getEvents&resultType=events&eventsConceptLang=eng&eventsCount=10&eventsEventImageCount=1&minArticlesInEvent=10&eventsIncludeEventSocialScore=true&conceptUri=' + conceptUri + '&eventsSortBy=rel&eventsPage=1', { jar: session }, function(error, response, body) {
+        if (error) callback(error, [ ]);
+        else if (JSON.parse(body).error !== undefined) {
+          callback(null, [ ]);
+        } else {
+          var json = JSON.parse(body);
+          callback(null, json.events.results);
+        }
+      });
+    },
+    function(callback) {
+      request('http://eventregistry.org/json/event?action=getEvents&resultType=events&eventsConceptLang=eng&eventsCount=10&eventsEventImageCount=1&minArticlesInEvent=10&eventsIncludeEventSocialScore=true&conceptUri=' + conceptUri + '&eventsSortBy=date&eventsPage=1', { jar: session }, function(error, response, body) {
+        if (error) callback(error, [ ]);
+        else if (JSON.parse(body).error !== undefined) {
+          callback(null, [ ]);
+        } else {
+          var json = JSON.parse(body);
+          callback(null, json.events.results);
+        }
+      });
+    }
+  ], function(err, r)
+  {
+    var out = { status: err == null ? 500 : 200, error: err, events: [ ]};
+    if (err)
+    {
+      return cb(out);
     }
     else {
-      var json = JSON.parse(body);
-      out.page = json.events.page;
-      out.pages = json.events.pages;
-      out.count = json.events.totalResults;
-      out.events = [ ];
-      // out.events = json.events.results;
-
       console.log("Analyzing events...");
-      // console.log(json.events.results);
+
+      //De-dup
+      var rCombined = r[0].concat(r[1]);
+      var rSet = new Set(rCombined);
+      var results = [...rSet];
 
       //Rate each event and append to array
-      var maxRelevance = json.events.results[0].wgt;
-      async.eachLimit(json.events.results, 8, function(event, asynccb)
+      var maxRelevance = results[0].wgt;
+      async.eachLimit(results, 8, function(event, asynccb)
       {
         //Get metadata
         if (event.warning === undefined)
@@ -37,13 +54,21 @@ exports.getEvents = function(session, conceptUri, lang, cb)
           var eventout = { };
           eventout.uri = event.uri;
           eventout.title = event.title[lang] || event.title["eng"] || null;
+          eventout.time = moment(event.eventDate, "YYYY-MM-DD").fromNow();
+          if (eventout.time.indexOf("in") > -1) eventout.time = "just now";
+          eventout.image = event.images[0] || null;
+
+          //Location simplification
           if (event.location === undefined || event.location == null || event.location.length == 0) { eventout.location = "" }
           else {
             eventout.location = event.location.label[lang] || event.location.label["eng"] || "";
             if (event.location.country !== undefined && event.location.country != null) eventout.location += ", " + (event.location.country.label[lang] || event.location.country.label["eng"] || "");
+            var matches = eventout.location.split(/\s*,\s*/);
+            if (matches.length > 2)
+            {
+              eventout.location = matches[0].trim() + ", " + matches[1].trim();
+            }
           }
-          eventout.time = moment(event.eventDate, "YYYY-MM-DD").fromNow();
-          eventout.image = event.images[0] || null;
 
           //Parse summary
           eventout.summary = event.summary[lang] || event.summary["eng"] || null;
@@ -68,14 +93,16 @@ exports.getEvents = function(session, conceptUri, lang, cb)
           var searchRelevance = event.wgt;
           var socialScore = event.socialScore;
           var timeDiffDays = Math.ceil(moment(new Date()).diff(moment(event.eventDate), 'days', true));
+          if (timeDiffDays < 0.1) { timeDiffDays = 0.1; }
           var recencyFactor = 4.92679 / ((timeDiffDays)^(0.432481));
+          if (recencyFactor > 50 || recencyFactor < 0) { recencyFactor = 50; }
           var relevanceFactor = searchRelevance / maxRelevance;
           var socialFactor = 0.017729 * ((socialScore)^(0.787861));
           var newsFactor = 0.017729 * ((eventout.articlesTotal)^(0.787861));
 
           eventout.hotness = Math.log10(recencyFactor * (socialFactor * newsFactor))*(100/2.5);
           eventout.relevance = eventout.hotness * relevanceFactor;
-          eventout.hotness = Math.round(eventout.hotness);
+          eventout.hotness = Math.ceil(eventout.hotness) + 3;
           console.log("SocBase: " + socialScore + " Time: " + recencyFactor + " Soc: " + socialFactor + " Articles: " + eventout.articlesTotal + " News: " + newsFactor +  " SocF: " + socialFactor + " Score: " + eventout.relevance);
 
           if (eventout.summary == null || eventout.relevance < 0.1 ||
@@ -93,7 +120,7 @@ exports.getEvents = function(session, conceptUri, lang, cb)
 
         //Sort array
         out.events = out.events.sort(function(e1, e2) {
-        	return e2.relevance - e1.relevance;
+          return e2.relevance - e1.relevance;
         });
 
         //Return
